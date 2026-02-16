dd4c07cfa357ee78fb308873530dd2b08ad0a4e0
Shobhit Sharma <shobhitsharma92in@gmail.com>
2026-02-16 01:06:10 -0800

    fix: handler.py

--- handler.py
+++ handler.py
@@ -1,18 +1,15 @@
 import runpod
 
-try:
-    from src.rp_handler import run
-except ImportError:
-    # In the container image, src files are copied to "/" so rp_handler is top-level.
-    from rp_handler import run
-
-
 def handler(job):
     job_input = job["input"]
-    normalized_job = {
-        "id": job.get("id", "local-test-job"),
-        "input": job_input
-    }
+    normalized_job = {"id": job.get("id", "local-test-job"), "input": job_input}
+
+    try:
+        from src.rp_handler import run
+    except ImportError:
+        # In container images using `COPY src .`, rp_handler is top-level.
+        from rp_handler import run
+
     return run(normalized_job)
 
 
